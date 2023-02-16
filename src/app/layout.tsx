4efb6a7f87ed7e3ae87f57fe3a3f1de27541ac85
danmakowiesky <dvm1993@hotmail.com>
2023-02-16 14:10:02 -0300

    arrumando lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,6 @@ import './globals.css'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>
         {children} <h1>teste</h1>
